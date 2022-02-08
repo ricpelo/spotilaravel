d085a99fbd51926cc9045013ea02742edfc01f84
Ricardo Pérez <ricpelo@gmail.com>
2022-02-08 20:29:45 +0100

    Añade los temas

---
+++ database/migrations/2022_02_08_192325_create_temas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Grammars\Grammar;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Grammar::macro('typeInterval', function () {
+            return 'interval';
+        });
+
+        Schema::create('temas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->addColumn('interval', 'duracion');
+            $table->foreignId('album_id')->constrained('albumes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('temas');
+    }
+};
